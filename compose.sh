f1fed38387ec220168a589b021fd0e16f74f8b2a
Ionut Balutoiu <ionut@balutoiu.com>
2026-01-26 19:38:23 +0200

    Update docker compose deployment
    * Add compose.sh script to simplify docker-compose commands
    * Use versioned images from GitHub Container Registry
    * Remove nginx config in favor of environment variable configuration
    * Update .gitignore to exclude .env and *.local.yaml files

---
+++ compose.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+DEFAULT_FILES_BROWSER_BACKEND_VERSION="0.11.0"
+DEFAULT_FILES_BROWSER_FRONTEND_VERSION="0.1.0"
+
+export BACKEND_IMAGE_TAG="${BACKEND_IMAGE_TAG:-$DEFAULT_FILES_BROWSER_BACKEND_VERSION}"
+export FRONTEND_IMAGE_TAG="${FRONTEND_IMAGE_TAG:-$DEFAULT_FILES_BROWSER_FRONTEND_VERSION}"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+COMPOSE_ARGS="-f ${DIR}/docker-compose.yaml"
+if [[ -f "${DIR}/docker-compose.local.yaml" ]]; then
+  COMPOSE_ARGS="$COMPOSE_ARGS -f ${DIR}/docker-compose.local.yaml"
+fi
+
+docker-compose $COMPOSE_ARGS "$@"
